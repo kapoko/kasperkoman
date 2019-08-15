e1827aa24f23410e3c1f89700adbb14170f636a6
Kasper Koman <kasper.koman@gmail.com>
2019-08-15 18:57:09 +0200

    Code cleanup

--- db/backup.sh
+++ db/backup.sh
@@ -45,31 +45,32 @@ dump)
     ;;
 restore)    
     echo "Restoring database to server"
-    if [ -d "./backups/$2" ]; then
-        echo "Backup found, backup up to server"
-        mongorestore "${params[@]}" \
-            --authenticationDatabase strapi \
-            --nsInclude 'strapi.*' \
-            --drop ./backups/$2/
-        if [ $? -eq 0 ]; then
-            echo "Succesfully restored database on remote"
-        else
-            echo $?
-            exit
-        fi
-
-        # Restore uploads folder
-        echo "Restoring uploads folder"
-        rsync -chavzPq -e ssh ./backups/uploads/ $SSH_USER@$SSH_HOST:$REMOTE_DIR/volumes/uploads
-        if [ $? -eq 0 ]; then
-            echo "All OK."
-        else
-            echo $?
-            exit
-        fi
-    else 
+    if [ ! -d "./backups/$2" ]; then
         echo "Backup does not exist. Available backups are:"
         ls -d ./backups/*/ | xargs -n 1 basename | awk '{print "- "$1}'
+        exit
+    fi
+
+    echo "Backup found, backup up to server"
+    mongorestore "${params[@]}" \
+        --authenticationDatabase strapi \
+        --nsInclude 'strapi.*' \
+        --drop ./backups/$2/
+    if [ $? -eq 0 ]; then
+        echo "Succesfully restored database on remote"
+    else
+        echo $?
+        exit
+    fi
+
+    # Restore uploads folder
+    echo "Restoring uploads folder"
+    rsync -chavzPq -e ssh ./backups/uploads/ $SSH_USER@$SSH_HOST:$REMOTE_DIR/volumes/uploads
+    if [ $? -eq 0 ]; then
+        echo "All OK."
+    else
+        echo $?
+        exit
     fi
     ;;
 *)
